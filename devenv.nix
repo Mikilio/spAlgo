71520ee0d425ffa6ff59e7d7eec8ff04325e6ee0
Mikilio <official.mikilio@gmail.com>
2024-03-04 13:34:56 +0100

    chore: configure devenv

---
+++ devenv.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+  enterShell = ''
+    hello
+    cargo --version
+  '';
+
+  # https://devenv.sh/languages/
+  languages.nix.enable = true;
+  languages.python.enable = true;
+  languages.python.version = "3.12.2";
+
+  languages.rust.enable = true;
+  languages.rust.channel = "stable";
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
