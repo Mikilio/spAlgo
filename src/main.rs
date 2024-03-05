4a7d848c46de8a18cbd62aaafd50d8472a4b8aab
Mikilio <official.mikilio@gmail.com>
2024-03-05 16:25:02 +0100

    fix: fixed parsing error
    - added improved error-handling
    - changed Edge type to hold larger values

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use std::{
     fs::File,
-    io::{self, BufRead, BufReader},
+    io::{BufRead, BufReader},
     path::Path,
     str::FromStr,
 };
 
 struct Edge {
-    from: u16,
-    to: u16,
+    from: u32,
+    to: u32,
     weight: u32,
 }
 
@@ -17,12 +17,12 @@ pub enum EdgeErrorKind {
     NoDataRow,
     InvalidFormat,
     InvalidValue,
-    Zero,
 }
 
 #[derive(Debug, PartialEq, Eq)]
 struct ParseEdgeError {
     kind: EdgeErrorKind,
+    line: String,
 }
 
 impl FromStr for Edge {
@@ -30,34 +30,28 @@ impl FromStr for Edge {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut fields = s.split_whitespace();
-        fields.next();
+        let Some("a") = fields.next() else {
+            return Err(ParseEdgeError {
+                kind: EdgeErrorKind::NoDataRow,
+                line: String::from_str(&s).unwrap(),
+            });
+        };
         let from = fields.next();
         let to = fields.next();
         let weight = fields.next();
         match (from, to, weight) {
             (Some(f), Some(t), Some(w)) => {
-                match (u16::from_str(f), u16::from_str(t), u32::from_str(w)) {
+                match (u32::from_str(f), u32::from_str(t), u32::from_str(w)) {
                     (Ok(from), Ok(to), Ok(weight)) => Ok(Edge { from, to, weight }),
                     _ => Err(ParseEdgeError {
                         kind: EdgeErrorKind::InvalidValue,
+                        line: String::from_str(&s).unwrap(),
                     }),
                 }
             }
             _ => Err(ParseEdgeError {
                 kind: EdgeErrorKind::InvalidFormat,
-            }),
-        }
-    }
-}
-
-impl TryFrom<Result<String, io::Error>> for Edge {
-    type Error = ParseEdgeError;
-
-    fn try_from(item: Result<String, io::Error>) -> Result<Self, Self::Error> {
-        match item {
-            Ok(s) => s.parse::<Edge>(),
-            _ => Err(ParseEdgeError {
-                kind: EdgeErrorKind::InvalidFormat,
+                line: String::from_str(&s).unwrap(),
             }),
         }
     }
@@ -73,7 +67,19 @@ fn load_graph(path: &Path) -> Vec<Edge> {
     // Read the file contents into a string, returns `io::Result<usize>`
     BufReader::new(file)
         .lines()
-        .filter_map(|line| Edge::try_from(line).ok())
+        .filter_map(|line| match Edge::from_str(&line.ok()?) {
+            Ok(e) => Some(e),
+            Err(err) => {
+                if err.kind != EdgeErrorKind::NoDataRow {
+                    panic!(
+                        "couldn't parse line:\n{}\nbecause of: {:#?}",
+                        err.line, err.kind
+                    )
+                } else {
+                    return None;
+                }
+            }
+        })
         .collect()
 }
 
