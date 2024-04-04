6be7a3b0adf947a6fd0fae718bc3fc0709c684f0
Mikilio <official.mikilio@gmail.com>
2024-04-04 16:08:33 +0200

    feat: completed implementation of simple heaps
    These are d-ary heaps that implement a priority queue without a decreare
    key operation.

--- src/implicit_heaps.rs
+++ src/implicit_heaps.rs
@@ -1,5 +1,3 @@
-use std::usize;
-
 use crate::dijkstra::Dijkstra;
 use crate::dijkstra::PriorityQueue;
 use crate::dimacs::*;
@@ -14,21 +12,21 @@ pub trait ImplicitHeap: Lookup {
     fn bubble_down(&mut self);
 }
 
-pub trait ImplicitHeapSimple: Dijkstra<Vec<Vertex>> {
-    fn bubble_up(&mut self, dirt: usize);
+pub trait ImplicitHeapSimple: Dijkstra<Vec<(Vertex,u32)>> {
+    fn bubble_up(&mut self);
     fn bubble_down(&mut self);
 }
 
 macro_rules! dijkstra_trait {
-    ($T:ident) => {
-        impl Dijkstra<Vec<Vertex>> for $T {
+    ($T:ident,$item:ty) => {
+        impl Dijkstra<Vec<$item>> for $T {
             fn new(n: usize, source: Vertex) -> Self {
                 $T::new(n, source)
             }
-            fn get_inner(&self) -> &Vec<Vertex> {
+            fn get_inner(&self) -> &Vec<$item> {
                 &self.inner
             }
-            fn get_mut_inner(&mut self) -> &mut Vec<Vertex> {
+            fn get_mut_inner(&mut self) -> &mut Vec<$item> {
                 &mut self.inner
             }
             fn get_dist(&self, v: Vertex) -> u32 {
@@ -56,7 +54,7 @@ macro_rules! dijkstra_trait {
 macro_rules! implicit_heap_simple {
     ($k:expr, $T:ident) => {
         pub struct $T {
-            inner: Vec<Vertex>,
+            inner: Vec<(Vertex,u32)>,
             dist: Vec<u32>,
             prev: Vec<Vertex>,
             seen: Vec<bool>,
@@ -64,9 +62,9 @@ macro_rules! implicit_heap_simple {
 
         impl $T {
             pub fn new(n: usize, source: Vertex) -> Self {
-                let mut inner: Vec<Vertex> = Vec::new();
+                let mut inner: Vec<(Vertex,u32)> = Vec::new();
                 let mut dist = vec![u32::MAX; n];
-                inner.push(source);
+                inner.push((source,0));
                 dist[usize::from(source)] = 0;
                 Self {
                     inner,
@@ -77,19 +75,19 @@ macro_rules! implicit_heap_simple {
             }
         }
 
-        dijkstra_trait!($T);
+        dijkstra_trait!($T, (Vertex,u32));
 
         impl ImplicitHeapSimple for $T {
-            fn bubble_up(&mut self, dirt: usize) {
-                let mut child = dirt;
+            fn bubble_up(&mut self) {
+                let mut child = self.get_inner().len() - 1;
 
                 let mut parent;
                 while child > 0 {
                     let heap = self.get_inner();
                     parent = (child - 1) / $k;
-                    let parent_v = heap[parent];
-                    let child_v = heap[child];
-                    if self.get_dist(parent_v) <= self.get_dist(child_v) {
+                    let (_,dist_parent) = heap[parent];
+                    let (_,dist_child) = heap[child];
+                    if dist_parent <= dist_child {
                         break;
                     }
                     self.get_mut_inner().swap(parent, child);
@@ -107,7 +105,10 @@ macro_rules! implicit_heap_simple {
                     let base = parent * $k + 1;
                     let end = usize::min(base + $k, n);
                     child = (base..end).reduce(|left, right| {
-                        if self.get_dist(heap[left]) > self.get_dist(heap[right]) {
+                    let (_,dist_left) = heap[left];
+                    let (_,dist_right) = heap[right];
+
+                        if dist_left > dist_right{
                             right
                         } else {
                             left
@@ -116,15 +117,48 @@ macro_rules! implicit_heap_simple {
                     child != None
                 } {
                     let child = child.unwrap();
-                    let parent_v = self.get_inner()[parent];
-                    let child_v = self.get_inner()[child];
-                    if self.get_dist(parent_v) <= self.get_dist(child_v) {
+                    let (_,dist_parent) = self.get_inner()[parent];
+                    let (_,dist_child) = self.get_inner()[child];
+                    if dist_parent <= dist_child {
                         break;
                     }
-
                     self.get_mut_inner().swap(parent, child);
                     parent = child;
                 }
+            }
+        }
+
+        impl PriorityQueue for $T {
+            fn new(n: usize, source: Vertex) -> Self {
+                $T::new(n, source)
+            }
+            fn explore(&mut self, e: &Edge) {
+                let alt = self.get_dist(e.from) + e.weight;
+                if !self.expanded(e.to) && alt < self.get_dist(e.to) {
+                    self.get_mut_inner().push((e.to,alt));
+                    self.set_dist(e.to, alt);
+                    self.bubble_up();
+                    self.set_prev(e.to, e.from);
+                }
+            }
+
+            fn pop_min(&mut self) -> Vertex {
+                let (min,_) = self.get_mut_inner().swap_remove(0);
+                self.bubble_down();
+                self.mark_seen(min);
+                while {
+                    !self.empty() && {
+                        let (maybe_min,_) = self.get_mut_inner()[0];
+                        self.expanded(maybe_min)
+                    }
+                } {
+                    self.get_mut_inner().swap_remove(0);
+                    self.bubble_down();
+                }
+                return min;
+            }
+            fn empty(&mut self) -> bool {
+                self.get_mut_inner().is_empty()
             }
         }
     };
@@ -156,7 +190,7 @@ macro_rules! implicit_heap {
             }
         }
 
-        dijkstra_trait!($T);
+        dijkstra_trait!($T,Vertex);
 
         impl Lookup for $T {
             fn lookup(&self, v: Vertex) -> usize {
@@ -219,6 +253,43 @@ macro_rules! implicit_heap {
                 }
             }
         }
+
+        impl PriorityQueue for $T {
+            fn new(n: usize, source: Vertex) -> Self {
+                $T::new(n, source)
+            }
+            fn explore(&mut self, e: &Edge) {
+                let alt = self.get_dist(e.from) + e.weight;
+                if !self.expanded(e.to) && alt < self.get_dist(e.to) {
+                    self.set_dist(e.to, e.weight);
+                    if self.get_prev(e.to) == UNDEFINED {
+                        self.get_mut_inner().push(e.to);
+                        let end = self.get_inner().len() - 1;
+                        self.update(e.to, end);
+                        self.bubble_up(end);
+                    } else {
+                        self.bubble_up(self.lookup(e.to));
+                    }
+                    self.set_prev(e.to, e.from);
+                }
+            }
+
+            fn pop_min(&mut self) -> Vertex {
+                let heap = self.get_mut_inner();
+                let last = heap
+                    .last()
+                    .expect("pop_min called even though heap was empty")
+                    .clone();
+                self.update(last, 0);
+                let min = self.get_mut_inner().swap_remove(0);
+                self.bubble_down();
+                self.mark_seen(min);
+                return min;
+            }
+            fn empty(&mut self) -> bool {
+                self.get_mut_inner().is_empty()
+            }
+        }
     };
 }
 
@@ -232,43 +303,6 @@ implicit_heap!(4, PentaryHeap);
 implicit_heap!(8, OctaryHeap);
 implicit_heap!(16, HexadecimaryHeap);
 
-impl<H: ImplicitHeap> PriorityQueue for H {
-    fn new(n: usize, source: Vertex) -> Self {
-        H::new(n, source)
-    }
-    fn explore(&mut self, e: &Edge) {
-        let alt = self.get_dist(e.from) + e.weight;
-        if !self.expanded(e.to) && alt < self.get_dist(e.to) {
-            self.set_dist(e.to, e.weight);
-            if self.get_prev(e.to) == UNDEFINED {
-                self.get_mut_inner().push(e.to);
-                let end = self.get_inner().len() - 1;
-                self.update(e.to, end);
-                self.bubble_up(end);
-            } else {
-                self.bubble_up(self.lookup(e.to));
-            }
-            self.set_prev(e.to, e.from);
-        }
-    }
-
-    fn pop_min(&mut self) -> Vertex {
-        let heap = self.get_mut_inner();
-        let last = heap
-            .last()
-            .expect("pop_min called even though heap was empty")
-            .clone();
-        self.update(last, 0);
-        let min = self.get_mut_inner().swap_remove(0);
-        self.bubble_down();
-        self.mark_seen(min);
-        return min;
-    }
-    fn empty(&mut self) -> bool {
-        self.get_mut_inner().is_empty()
-    }
-}
-
 #[cfg(test)]
 mod tests {
 
@@ -281,7 +315,7 @@ mod tests {
         ($name:ident,$T:ident) => {
             #[test]
             fn $name() {
-                let n = 10000;
+                let n = 1000000;
                 let mut highest_min = 0;
                 let mut vertices = $T::new(n, Vertex(1));
                 let mut rng = thread_rng();
@@ -293,7 +327,7 @@ mod tests {
                         to,
                     });
                 }
-                //decrease_key
+                //decrease_key or extra inserts
                 for _ in 0..n {
                     let from = Vertex(1);
                     let to: Vertex = rng.gen_range(1..n).try_into().unwrap();
@@ -318,4 +352,9 @@ mod tests {
     push_pop_test!(push_pop_4, PentaryHeap);
     push_pop_test!(push_pop_8, OctaryHeap);
     push_pop_test!(push_pop_16, HexadecimaryHeap);
+
+    push_pop_test!(push_pop_2_simple, BinaryHeapSimple);
+    push_pop_test!(push_pop_4_simple, PentaryHeapSimple);
+    push_pop_test!(push_pop_8_simple, OctaryHeapSimple);
+    push_pop_test!(push_pop_16_simple, HexadecimaryHeapSimple);
 }
